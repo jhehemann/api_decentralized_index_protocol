685a7cb07a2e40aa90745a01a2b860c2d2b7b1d6
Jannik Hehemann <jannik@hehemann-melle.de>
2022-11-11 00:50:01 +0100

    Created app with first services

---
+++ src/app.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { Contract, ethers } from 'ethers';
+import { ConstructorFragment } from 'ethers/lib/utils';
+import * as TokenContractJson from './assets/IndexToken.json';
+import * as IndexContractJson from './assets/IndexContract.json'
+
+const TOKEN_CONTRACT_ADDRESS = '0x90c84237fddf091b1e63f369af122eb46000bc70';
+const INDEX_CONTRACT_ADDRESS = '0x3D63c50AD04DD5aE394CAB562b7691DD5de7CF6f';
+
+
+
+
+@Injectable()
+export class AppService {
+
+  provider: ethers.providers.Provider;
+  tokenContract: Contract;
+  indexContract: Contract;
+
+  constructor() {
+    this.provider = ethers.getDefaultProvider('http://127.0.0.1:8545/');
+    this.tokenContract = new ethers.Contract(TOKEN_CONTRACT_ADDRESS, TokenContractJson.abi, this.provider);
+    this.indexContract = new ethers.Contract(INDEX_CONTRACT_ADDRESS, IndexContractJson.abi, this.provider);
+  }
+
+  async getTotalSupply() {
+    const totalSupplyBN = await this.tokenContract.totalSupply();
+    const totalSupply = ethers.utils.formatEther(totalSupplyBN);
+    return totalSupply;
+
+  }
+
+}
+ 
